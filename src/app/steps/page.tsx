dec5b3e7de7b16b747bd65eedbf481d1732dc3fb
Monica Carvajal <monik.182@gmail.com>
2024-12-29 02:40:53 +0100

    - refactor steps page - create mock step1 page

--- src/app/steps/page.tsx
+++ src/app/steps/page.tsx
@@ -1,34 +1,56 @@
-import { Button, Group } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Group } from '@chakra-ui/react';
 import { StepsCompletedContent, StepsContent, StepsItem, StepsList, StepsNextTrigger, StepsPrevTrigger, StepsRoot } from '../../components/ui/steps';
+import { Step1 } from './step1';
 
 export default function Steps() {
+
+  const steps = [
+    { title: 'Define Vision', content: <Step1 /> },
+    { title: '3-Year Milestone', content: 'Step 2' },
+    { title: 'Set Goals', content: 'Step 3' },
+    { title: 'Prioritize', content: 'Step 4' },
+    { title: 'Focus on One', content: 'Step 5' },
+    { title: 'Plan Actions & Metrics', content: 'Step 6' },
+    // { title: 'Set Indicators', content: 'Step 7' },
+    { title: 'Pick a Start Date', content: 'Step 7' },
+  ];
+
   return (
-    <StepsRoot key="subtle" variant="subtle" count={3}>
-      <StepsList>
-        <StepsItem index={0} title="Step 1" />
-        <StepsItem index={1} title="Step 2" />
-        <StepsItem index={2} title="Step 3" />
-      </StepsList>
+    <StepsRoot key="subtle" variant="subtle" count={steps.length} height="calc(80vh - 2rem)">
+      <Grid gridTemplateRows="10% 90% 10%" height="100%" gap="1rem">
+        <GridItem>
+          <StepsList>
+            {steps.map((step, index) => (
+              <StepsItem key={index} index={index} title={step.title} />
+            ))}
+          </StepsList>
+        </GridItem>
+        <GridItem>
 
-      <StepsContent index={0}>Step 1</StepsContent>
-      <StepsContent index={1}>Step 2</StepsContent>
-      <StepsContent index={2}>Step 3</StepsContent>
-      <StepsCompletedContent>
-        All steps are complete!
-      </StepsCompletedContent>
-
-      <Group>
-        <StepsPrevTrigger asChild>
-          <Button variant="outline" size="sm">
-            Prev
-          </Button>
-        </StepsPrevTrigger>
-        <StepsNextTrigger asChild>
-          <Button variant="outline" size="sm">
-            Next
-          </Button>
-        </StepsNextTrigger>
-      </Group>
+          {steps.map((step, index) => (
+            <StepsContent key={index} index={index}>
+              {step.content}
+            </StepsContent>
+          ))}
+          <StepsCompletedContent>
+            Congrats, you have created your 3-months plan!
+          </StepsCompletedContent>
+        </GridItem>
+        <GridItem>
+          <Group>
+            <StepsPrevTrigger asChild>
+              <Button variant="outline" size="sm">
+                Prev
+              </Button>
+            </StepsPrevTrigger>
+            <StepsNextTrigger asChild>
+              <Button variant="outline" size="sm">
+                Next
+              </Button>
+            </StepsNextTrigger>
+          </Group>
+        </GridItem>
+      </Grid>
     </StepsRoot>
   )
 }
---
+++ src/app/steps/step1.tsx
@@ -0,0 +1,10 @@
+import { Heading, Textarea } from '@chakra-ui/react';
+
+export function Step1() {
+  return (
+    <div>
+      <Heading size="2xl">Create your long term vision</Heading>
+      <Textarea variant="outline" placeholder="outline" />
+    </div>
+  )
+}
