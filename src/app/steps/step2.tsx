7ac26e06dc572b6930fa923bc282e633e833313f
Monica Carvajal <monik.182@gmail.com>
2024-12-29 15:31:38 +0100

    add resize prop to textarea component

--- src/app/steps/step2.tsx
+++ src/app/steps/step2.tsx
@@ -8,6 +8,7 @@ export function Step2() {
       description="Your 3-year milestone is your compass. Picture yourself three years from today—what accomplishments will make you proud and show you're on track for your ultimate vision? Be ambitious, but grounded. Identify what will make the biggest impact on your life, career, and growth in this transformative period. Dream big, act bold, and build momentum."
     >
       <Textarea
+        autoresize
         size="xl"
         variant="outline"
         placeholder="Where do you want to be in 3 years? What key achievements, progress, or changes will set the stage for your long-term vision? Think of this as the bridge between where you are now and where you're going."
