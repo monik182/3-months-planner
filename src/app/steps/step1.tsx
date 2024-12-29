96c30ccc35e28c7b0e5f0522130cc79b2d0e3e55
Monica Carvajal <monik.182@gmail.com>
2024-12-29 02:45:24 +0100

    update step 1

--- src/app/steps/step1.tsx
+++ src/app/steps/step1.tsx
@@ -4,7 +4,10 @@ export function Step1() {
   return (
     <div>
       <Heading size="2xl">Create your long term vision</Heading>
-      <Textarea variant="outline" placeholder="outline" />
+      <p>
+        Dare to dream without limits. Picture a future where you've achieved everything you’ve ever desired. Be bold and dream unapologetically—this is your life, your vision, your legacy. What passions have you followed fearlessly? What does fulfillment look like in your career, relationships, and personal growth? Envision a life where every choice you make aligns with your deepest values and aspirations. Let your imagination run free, embrace your wildest ambitions, and create a vision that excites and motivates you every day. Dream as if failure isn’t an option, and let your boldness pave the way.
+      </p>
+      <Textarea variant="outline" placeholder="Think big. What are the dreams you’ve always wanted to pursue? How would your life look if you reached your full potential? Be bold and dream unapologetically." />
     </div>
   )
 }
