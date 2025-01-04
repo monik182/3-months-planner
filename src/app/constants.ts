31d8276001ac3c6517db317e559c6eb8360c8157
Monica Carvajal <monik.182@gmail.com>
2025-01-04 01:17:53 +0100

    create date selector

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,18 @@
+import { Plan } from './types'
+import { calculateEndDate, getNextStartDate } from './util'
+
 export const WEEKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 export const DEFAULT_WEEKS = WEEKS.map((id) => id.toString())
 
 export const DEFAULT_STRATEGY = { id: '', value: '', weeks: [...DEFAULT_WEEKS], isEditing: false }
+
+export const DEFAULT_WEEKS_LIST = WEEKS.map((id) => ({ id: id.toString(), label: `Week ${id}`, value: id.toString() }))
+
+export const INITIAL_PLAN: Plan = {
+  vision: '',
+  threeYearMilestone: '',
+  goals: [],
+  startDate: getNextStartDate(),
+  endDate: calculateEndDate(getNextStartDate()),
+} 
---
+++ src/app/steps/Step4/DateSelector.tsx
@@ -0,0 +1,63 @@
+import { getNextStartDates } from '@/util';
+import { Flex, createListCollection, Text } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { SelectContent, SelectItem, SelectLabel, SelectRoot, SelectTrigger, SelectValueText } from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
+
+interface DateSelectorProps {
+  onChange: (value: string) => void
+  date: string
+}
+
+export function DateSelector({ onChange, date }: DateSelectorProps) {
+  const [value, setValue] = useState<string[]>([date])
+  const [checked, setChecked] = useState(false)
+
+  const dates = getNextStartDates(checked)
+
+  const options = dates.map(date => {
+    return {
+      value: date,
+      label: dayjs(date).format('MMMM DD, YYYY')
+    }
+  })
+
+  const optionsCollection = createListCollection({
+    items: options
+  })
+
+  const handleChange = (e: any) => {
+    setValue(e.value)
+    onChange(e.value[0])
+  }
+
+  useEffect(() => {
+    setValue([date])
+  }, [date])
+
+  return (
+    <Flex gap="1rem" align="end">
+      <SelectRoot
+        collection={optionsCollection}
+        value={value}
+        onValueChange={handleChange}
+      >
+        <SelectLabel>Start Date</SelectLabel>
+        <SelectTrigger>
+          <SelectValueText placeholder="Select start date" />
+        </SelectTrigger>
+        <SelectContent>
+          {optionsCollection.items.map((date) => (
+            <SelectItem item={date} key={date.value}>
+              {date.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </SelectRoot>
+      <Flex align="center" gap="5px">
+        <Switch size="xs" checked={checked} onCheckedChange={(e) => setChecked(e.checked)} /> <Text textStyle="xs">Start on Mondays</Text>
+      </Flex>
+    </Flex>
+  )
+}
