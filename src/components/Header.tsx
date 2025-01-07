167f886e148739c681a3a4139d1e441e73db10ff
Monica Carvajal <monik.182@gmail.com>
2025-01-07 21:04:26 +0100

    - create user avatar - add login/out links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,89 @@
 'use client'
-import { Flex, HStack, Heading, Separator } from '@chakra-ui/react'
+import { Flex, HStack, Heading, Separator, Text } from '@chakra-ui/react'
 import { ColorModeButton } from './ui/color-mode'
 import { SegmentedControl } from './ui/segmented-control'
-import { SlHome, SlNotebook } from 'react-icons/sl'
+import { SlHome, SlLogout, SlNotebook } from 'react-icons/sl'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useUser } from '@auth0/nextjs-auth0/client'
-
+import { Avatar } from './ui/avatar'
+import { RxDashboard } from 'react-icons/rx'
 
 const pageMap: Record<string, string> = {
-  home: '/dashboard',
+  home: '/',
+  dashboard: '/dashboard',
   planner: '/steps',
 }
 
-export function Header() {
-  const { user, error, isLoading } = useUser()
+const items = [
+  {
+    value: "home",
+    label: (
+      <HStack>
+        <SlHome />
+        Home
+      </HStack>
+    ),
+    hide: false,
+  },
+  {
+    value: "dashboard",
+    label: (
+      <HStack>
+        <RxDashboard />
+        Dashboard
+      </HStack>
+    ),
+    hide: true,
+  },
+  {
+    value: "planner",
+    label: (
+      <HStack>
+        <SlNotebook />
+        Planner
+      </HStack>
+    ),
+    hide: true,
+  },
+]
 
+export function Header() {
+  const { user } = useUser()
   const [value, setValue] = useState('home')
   const router = useRouter()
+  const filteredItems = items.map((item) => ({ ...item, hide: item.value === 'home' ? false : !user })).filter((item) => !item.hide)
+
   const handleOnChange = (value: string) => {
     setValue(value)
     router.push(pageMap[value])
   }
 
-  const items = [
-    {
-      value: "home",
-      label: (
-        <HStack>
-          <SlHome />
-          Home
-        </HStack>
-      ),
-    },
-    {
-      value: "planner",
-      label: (
-        <HStack>
-          <SlNotebook />
-          Planner
-        </HStack>
-      ),
-    },
-  ]
-
   return (
     <header>
       <Flex justify="space-between" align="center" marginTop="1rem">
         <Flex gap="1rem" align="center">
-          <Heading size="2xl">3-Months Planner</Heading>
+          <Heading size="2xl" onClick={() => handleOnChange('home')}>
+            3-Month Planner
+          </Heading>
           <SegmentedControl
             size="lg"
             value={value}
             onValueChange={(e) => handleOnChange(e.value)}
-            items={items}
+            items={filteredItems}
           />
         </Flex>
-        <Flex gap="5px" align="center">
+        <Flex gap="5px" alignItems="center">
           {user ? (
-            <a href="/api/auth/logout">Logout</a>
+            <Flex gap="1rem" alignItems="center">
+              <Avatar
+                name={user?.name || 'User'}
+                src={user?.picture || ''}
+                shape="full"
+                size="lg"
+              />
+              <a href="/api/auth/logout" className="flex flex-col justify-center items-center gap-2"><SlLogout /> <Text textStyle="xs">Logout</Text></a>
+            </Flex>
           ) : (
             <a href="/api/auth/login">Login</a>
           )}
