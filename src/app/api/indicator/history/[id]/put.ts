65ff51350c6fd78bf8b9a6b1c3b500c3449cb753
Monica Carvajal <monik.182@gmail.com>
2025-01-12 18:19:40 +0100

    create indicator history apis

---
+++ src/app/api/indicator/history/[id]/put.ts
@@ -0,0 +1,22 @@
+import { formatError } from '@/lib/prismaHandler'
+import { NextRequest } from 'next/server'
+import { indicatorHistoryHandler } from '@/db/prismaHandler'
+import { SegmentData } from '@/app/types'
+import { PartialIndicatorHistorySchema } from '@/lib/validators/indicatorHistory'
+
+export async function PUT(request: NextRequest, segmentData: SegmentData) {
+  const params = await segmentData.params
+  const data = await request.json()
+
+  if (!data) {
+    return new Response('Invalid data', { status: 400 })
+  }
+
+  try {
+    const parsedData = PartialIndicatorHistorySchema.parse(data)
+    const indicator = await indicatorHistoryHandler.update(params.id, parsedData)
+    return new Response(JSON.stringify(indicator), { status: 200 })
+  } catch (error) {
+    return new Response(formatError(error), { status: 500 })
+  }
+}
---
+++ src/app/api/indicator/history/get.ts
@@ -0,0 +1,19 @@
+import { formatError } from '@/lib/prismaHandler'
+import { NextRequest } from 'next/server'
+import { indicatorHistoryHandler } from '@/db/prismaHandler'
+import { SegmentData } from '@/app/types'
+
+export async function GET(_: NextRequest, segmentData: SegmentData) {
+  const params = await segmentData.params
+
+  if (!params.id) {
+    return new Response('Invalid indicator id', { status: 400 })
+  }
+
+  try {
+    const response = await indicatorHistoryHandler.findOne(params.id)
+    return new Response(JSON.stringify(response), { status: 200 })
+  } catch (error) {
+    return new Response(formatError(error), { status: 500 })
+  }
+}
