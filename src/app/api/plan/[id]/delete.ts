f7b90deee644daa71d12f5a25d58ec2fff51d9c4
Monica Carvajal <monik.182@gmail.com>
2025-01-12 18:13:32 +0100

    create delete plan api

---
+++ src/app/api/plan/[id]/delete.ts
@@ -0,0 +1,15 @@
+import { formatError } from '@/lib/prismaHandler'
+import { NextRequest } from 'next/server'
+import { plansHandler } from '@/db/prismaHandler'
+import { SegmentData } from '@/app/types'
+
+export async function DELETE(request: NextRequest, segmentData: SegmentData) {
+  const params = await segmentData.params
+
+  try {
+    const response = await plansHandler.delete(params.id)
+    return new Response(JSON.stringify(response), { status: 200 })
+  } catch (error) {
+    return new Response(formatError(error), { status: 500 })
+  }
+}
