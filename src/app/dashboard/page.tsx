d903bd6c2e7d961a3664bec086a6cd240cfc04b1
Monica Carvajal <monik.182@gmail.com>
2025-01-14 22:31:35 +0100

    delete deprecated components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,27 @@
 'use client'
 import { Box, Grid, HStack, Heading, Tabs, Text } from '@chakra-ui/react'
-import { Week } from './Week/Week'
-import { getChartData, getCurrentWeekFromStartDate } from '@/app/util'
+import { getCurrentWeekFromStartDate } from '@/app/util'
 import { DEFAULT_WEEKS } from '@/app/constants'
 import { MdCelebration } from 'react-icons/md'
 import { ProgressBar, ProgressRoot, ProgressValueText } from '@/components/ui/progress'
 import { PlanProvider, usePlanContext } from '@/app/providers/usePlanContext'
 import dayjs from 'dayjs'
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
-import { useProtectedPage } from '@/app/hooks/useProtectedPage'
-import { useState } from 'react'
 
 function Dashboard() {
-  const { planActions } = usePlanContext()
-  const { data: currentPlan, isLoading } = planActions.useGetCurrent()
-  console.log('Current plan from actions', currentPlan)
-  const endOfYPlan = dayjs(currentPlan.endDate).format('DD MMMM YYYY')
-  const currentWeek = getCurrentWeekFromStartDate(currentPlan.startDate)
+  const { planActions, goalHistoryActions } = usePlanContext()
+  const { data: currentPlan, isLoading } = planActions.useGet()
+  const { data: goalHist } = goalHistoryActions.useGetByPlanId(currentPlan?.id as string)
+  // console.log('Current plan from actions', currentPlan)
+  // console.log('Current GOAL goalHist from plan from actions', goalHist)
+  const today = dayjs().format('DD MMMM YYYY')
+  const endOfYPlan = dayjs(currentPlan?.endDate).format('DD MMMM YYYY')
+  const currentWeek = getCurrentWeekFromStartDate(currentPlan?.startDate as Date)
+  console.log({
+    endOfYPlan,
+    currentPlan,
+    start: currentPlan?.startDate
+  })
   // const data = getChartData(planTracker)
 
   if (isLoading) {
@@ -35,60 +40,60 @@ function Dashboard() {
     )
   }
   
-  return (
-    <div>
-      {currentPlan.id}
-    </div>
-  )
-
-
   // return (
-  //   <Grid>
-  //     <Grid gap="1rem" gridTemplateColumns="30% 70%" padding="1rem 0" alignItems="center">
-  //       <Box>
-  //         <Heading size="4xl">Week {currentWeek}</Heading>
-  //         <Box>
-  //           <Grid gridTemplateColumns="80% 20%" gap="1rem" alignItems="center">
-  //             <ProgressRoot striped animated colorPalette="cyan">
-  //               <HStack gap="5">
-  //                 <ProgressBar flex="1" />
-  //                 <ProgressValueText>{currentWeek}/12</ProgressValueText>
-  //               </HStack>
-  //             </ProgressRoot>
-  //             <MdCelebration />
-  //           </Grid>
-  //         </Box>
-  //         <Text><b>End of year:</b> {endOfYPlan}</Text>
-  //         <Text><b>Today:</b> {today}</Text>
-  //       </Box>
-  //       <LineChart width={1000} height={300} data={data}
-  //         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-  //         <CartesianGrid strokeDasharray="3 3" />
-  //         <XAxis dataKey="label" interval={0} />
-  //         <YAxis domain={[0, 100]} ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} />
-  //         <Tooltip />
-  //         <Legend formatter={(value) => value.toUpperCase()} />
-  //         <Line type="monotone" dataKey="score" stroke="#8884d8" />
-  //       </LineChart>
-  //     </Grid>
-
-  //     <Box marginTop="2rem">
-  //       <Tabs.Root lazyMount unmountOnExit defaultValue={`tab-${currentWeek}`} fitted variant="subtle">
-  //         <Tabs.List>
-  //           {DEFAULT_WEEKS.map((week) => (
-  //             <Tabs.Trigger key={`week-${week}`} value={`tab-${week}`}>W-{week}</Tabs.Trigger>
-  //           ))}
-  //           <Tabs.Indicator rounded="l2" />
-  //         </Tabs.List>
-  //         {planTracker.weeks.map((week) => (
-  //           <Tabs.Content key={week.id} value={`tab-${week.weekNumber}`}>
-  //             <Week week={week} />
-  //           </Tabs.Content>
-  //         ))}
-  //       </Tabs.Root>
-  //     </Box>
-  //   </Grid>
+  //   <div>
+  //     {currentPlan.id}
+  //   </div>
   // )
+
+
+  return (
+    <Grid>
+      <Grid gap="1rem" gridTemplateColumns="30% 70%" padding="1rem 0" alignItems="center">
+        <Box>
+          <Heading size="4xl">Week {currentWeek}</Heading>
+          <Box>
+            <Grid gridTemplateColumns="80% 20%" gap="1rem" alignItems="center">
+              <ProgressRoot striped animated colorPalette="cyan">
+                <HStack gap="5">
+                  <ProgressBar flex="1" />
+                  <ProgressValueText>{currentWeek}/12</ProgressValueText>
+                </HStack>
+              </ProgressRoot>
+              <MdCelebration />
+            </Grid>
+          </Box>
+          <Text><b>End of year:</b> {endOfYPlan}</Text>
+          <Text><b>Today:</b> {today}</Text>
+        </Box>
+        {/* <LineChart width={1000} height={300} data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="label" interval={0} />
+          <YAxis domain={[0, 100]} ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} />
+          <Tooltip />
+          <Legend formatter={(value) => value.toUpperCase()} />
+          <Line type="monotone" dataKey="score" stroke="#8884d8" />
+        </LineChart> */}
+      </Grid>
+
+      <Box marginTop="2rem">
+        <Tabs.Root lazyMount unmountOnExit defaultValue={`tab-${currentWeek}`} fitted variant="subtle">
+          <Tabs.List>
+            {DEFAULT_WEEKS.map((week) => (
+              <Tabs.Trigger key={`week-${week}`} value={`tab-${week}`}>W-{week}</Tabs.Trigger>
+            ))}
+            <Tabs.Indicator rounded="l2" />
+          </Tabs.List>
+          {/* {planTracker.weeks.map((week) => (
+            <Tabs.Content key={week.id} value={`tab-${week.weekNumber}`}>
+              <Week week={week} />
+            </Tabs.Content>
+          ))} */}
+        </Tabs.Root>
+      </Box>
+    </Grid>
+  )
 }
 
 
