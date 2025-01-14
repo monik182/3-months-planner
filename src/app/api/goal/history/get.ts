069798e8ac0cf91cd834dde414ed8aecd5ac2357
Monica Carvajal <monik.182@gmail.com>
2025-01-14 22:26:34 +0100

    fix

--- src/app/api/goal/history/get.ts
+++ src/app/api/goal/history/get.ts
@@ -3,14 +3,11 @@ import { NextRequest } from 'next/server'
 import { goalHistoryHandler } from '@/db/prismaHandler'
 
 export async function GET(request: NextRequest) {
-  const goalId = request.nextUrl.searchParams.get('goalId')
-
-  if (!goalId) {
-    return new Response('Invalid goal history id', { status: 400 })
-  }
+  const goalId = request.nextUrl.searchParams.get('goalId') ?? undefined
+  const planId = request.nextUrl.searchParams.get('planId') ?? undefined
 
   try {
-    const response = await goalHistoryHandler.findMany({ goalId })
+    const response = await goalHistoryHandler.findMany({ goalId, planId })
     return new Response(JSON.stringify(response), { status: 200 })
   } catch (error) {
     return new Response(formatError(error), { status: 500 })
