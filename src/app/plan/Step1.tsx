ed9f0a06ef1e110f1a4b99027a348200dec4623a
Monica Carvajal <monik.182@gmail.com>
2025-01-17 00:32:08 +0100

    integrate plan update to steps 1 and 2

--- src/app/plan/Step1.tsx
+++ src/app/plan/Step1.tsx
@@ -1,4 +1,4 @@
-import { Textarea } from '@chakra-ui/react'
+import { Flex, Spinner, Textarea } from '@chakra-ui/react'
 import { StepLayout } from './stepLayout'
 import { Step, Vision } from '@/app/types/types'
 import { useState } from 'react'
@@ -6,13 +6,14 @@ import { usePlanContext } from '@/app/providers/usePlanContext'
 import { useDebouncedCallback } from 'use-debounce'
 
 export function Step1({ }: Step<Vision>) {
-  const { plan } = usePlanContext()
-  const [value, setValue] = useState(plan.plan.vision)
+  const { plan, planActions } = usePlanContext()
+  const [value, setValue] = useState(plan?.vision ?? '')
+  const update = planActions.useUpdate()
 
   const debounced = useDebouncedCallback(
     (vision: string) => {
-      plan.updatePlan({ vision })
-    }, 1000)
+      update.mutate({ planId: plan!.id, updates: { vision } })
+    }, 1500)
 
   const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const vision = e.target.value
@@ -25,14 +26,21 @@ export function Step1({ }: Step<Vision>) {
       title="Define your long term vision"
       description="Dare to dream without limits. Picture a future where you've achieved everything you’ve ever desired. Be bold and dream unapologetically—this is your life, your vision, your legacy. What passions have you followed fearlessly? What does fulfillment look like in your career, relationships, and personal growth? Envision a life where every choice you make aligns with your deepest values and aspirations. Let your imagination run free, embrace your wildest ambitions, and create a vision that excites and motivates you every day. Dream as if failure isn’t an option, and let your boldness pave the way."
     >
-      <Textarea
-        autoresize
-        size="xl"
-        variant="outline"
-        value={value}
-        onChange={handleOnChange}
-        placeholder="Think big. What are the dreams you’ve always wanted to pursue? How would your life look if you reached your full potential? Be bold and dream unapologetically."
-      />
+      <Flex direction="column" gap="1rem">
+        <Textarea
+          autoresize
+          size="xl"
+          variant="outline"
+          value={value}
+          onChange={handleOnChange}
+          placeholder="Think big. What are the dreams you’ve always wanted to pursue? How would your life look if you reached your full potential? Be bold and dream unapologetically."
+        />
+        <Flex gap="1rem" height="1rem" justifyContent="flex-end" alignItems="center">
+          {update.isPending && (
+            <><Spinner size="xs" /> Saving</>
+          )}
+        </Flex>
+      </Flex>
     </StepLayout>
   )
 }
--- src/app/plan/Step2.tsx
+++ src/app/plan/Step2.tsx
@@ -1,4 +1,4 @@
-import { Textarea } from '@chakra-ui/react'
+import { Flex, Spinner, Textarea } from '@chakra-ui/react'
 import { StepLayout } from './stepLayout'
 import { Step, Vision } from '@/app/types/types'
 import { useState } from 'react'
@@ -6,13 +6,15 @@ import { usePlanContext } from '@/app/providers/usePlanContext'
 import { useDebouncedCallback } from 'use-debounce'
 
 export function Step2({ }: Step<Vision>) {
-  const { plan } = usePlanContext()
-  const [value, setValue] = useState(plan.plan.milestone)
+  const { plan, planActions } = usePlanContext()
+  const [value, setValue] = useState(plan?.milestone ?? '')
+  const update = planActions.useUpdate()
+
 
   const debounced = useDebouncedCallback(
     (milestone: string) => {
-      plan.updatePlan({ milestone })
-    }, 1000)
+      update.mutate({ planId: plan!.id, updates: { milestone } })
+    }, 2000)
 
   const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const milestone = e.target.value
@@ -25,14 +27,21 @@ export function Step2({ }: Step<Vision>) {
       title="Define your 3-Year Milestone"
       description="Your 3-year milestone is your compass. Picture yourself three years from today—what accomplishments will make you proud and show you're on track for your ultimate vision? Be ambitious, but grounded. Identify what will make the biggest impact on your life, career, and growth in this transformative period. Dream big, act bold, and build momentum."
     >
-      <Textarea
-        autoresize
-        size="xl"
-        variant="outline"
-        value={value}
-        onChange={handleOnChange}
-        placeholder="Where do you want to be in 3 years? What key achievements, progress, or changes will set the stage for your long-term vision? Think of this as the bridge between where you are now and where you're going."
-      />
+      <Flex direction="column" gap="1rem">
+        <Textarea
+          autoresize
+          size="xl"
+          variant="outline"
+          value={value}
+          onChange={handleOnChange}
+          placeholder="Where do you want to be in 3 years? What key achievements, progress, or changes will set the stage for your long-term vision? Think of this as the bridge between where you are now and where you're going."
+        />
+        <Flex gap="1rem" height="1rem" justifyContent="flex-end" alignItems="center">
+          {update.isPending && (
+            <><Spinner size="xs" /> Saving</>
+          )}
+        </Flex>
+      </Flex>
     </StepLayout>
   )
 }
