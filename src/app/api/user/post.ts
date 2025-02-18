64f9105dbcc47bf4dda9bc40b54af383fc49cef8
Monica Carvajal <monik.182@gmail.com>
2025-02-17 19:34:41 -0500

    fix create user api

--- src/app/api/user/post.ts
+++ src/app/api/user/post.ts
@@ -1,6 +1,5 @@
-import { userHandler, waitlistHandler } from '@/db/prismaHandler'
-
-import { WaitlistSchema } from '@/lib/validators/waitlist'
+import { userHandler } from '@/db/prismaHandler'
+import { UserSchema } from '@/lib/validators/user'
 import { NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
@@ -11,14 +10,14 @@ export async function POST(request: NextRequest) {
   }
 
   try {
-    const parsedData = WaitlistSchema.parse(data)
-
-    const exists = await userHandler.findOne(data)
+    const exists = await userHandler.findOneByEmail(data.email)
 
     if (!!exists) {
       return new Response(JSON.stringify({ message: 'The user is already registered', ok: false }), { status: 400 })
     }
-    const response = await waitlistHandler.create(parsedData)
+
+    const parsedData = UserSchema.parse(data)
+    const response = await userHandler.create(parsedData)
     return new Response(JSON.stringify({ ...response, ok: true }), { status: 200 })
   } catch (error) {
     return new Response(JSON.stringify({ error, ok: false }), { status: 500 })
