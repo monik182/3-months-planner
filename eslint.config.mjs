0ad3cf19eed953d1104ce267c710a2faa288dcfc
Monica Carvajal <monik.182@gmail.com>
2025-02-18 16:40:22 -0500

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    extends: ['next'],
+    extends: ["next"],
+  }),
+  ...compat.config({
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
