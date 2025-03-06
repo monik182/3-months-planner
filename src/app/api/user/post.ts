4db3046fde8132da448449f681474027bdff79e6
Monica Carvajal <monik.182@gmail.com>
2025-03-05 21:56:42 -0500

    update user post route

--- src/app/api/user/post.ts
+++ src/app/api/user/post.ts
@@ -10,15 +10,15 @@ export async function POST(request: NextRequest) {
   }
 
   try {
-    const exists = await userHandler.findOneByEmail(data.email)
+    const existingUser = await userHandler.findOneByEmail(data.email)
 
-    if (!!exists) {
-      return new Response(JSON.stringify({ message: 'The user is already registered', ok: false }), { status: 400 })
+    if (!!existingUser) {
+      return new Response(JSON.stringify({ ...existingUser, ok: true }), { status: 200 })
     }
 
     const parsedData = UserSchema.parse(data)
     const response = await userHandler.create(parsedData)
-    return new Response(JSON.stringify({ ...response, ok: true }), { status: 200 })
+    return new Response(JSON.stringify({ ...response, ok: true }), { status: 201 })
   } catch (error) {
     return new Response(JSON.stringify({ error, ok: false }), { status: 500 })
   }
