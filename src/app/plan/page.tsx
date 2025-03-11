cea1b5fcb664521c21021bfc1649fe450a507204
Monica Carvajal <monik.182@gmail.com>
2025-03-11 15:05:43 -0500

    remove step2

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -2,7 +2,6 @@
 import { Button, Center, Flex, Grid, GridItem, Group, Spinner } from '@chakra-ui/react'
 import { StepsCompletedContent, StepsContent, StepsItem, StepsList, StepsNextTrigger, StepsPrevTrigger, StepsRoot } from '@/components/ui/steps'
 import { Step1 } from './Step1'
-import { Step2 } from './Step2'
 import { Step3 } from './Step3/Step3'
 import { Step4 } from './Step4/Step4'
 import { usePlanContext } from '@/app/providers/usePlanContext'
