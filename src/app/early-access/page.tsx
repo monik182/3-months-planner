dca308e9cea7e2039c794b5a59cfe3ae6e5b4838
Monica Carvajal <monik.182@gmail.com>
2025-05-06 14:03:21 +0200

    update signup route

--- src/app/early-access/page.tsx
+++ src/app/early-access/page.tsx
@@ -31,7 +31,7 @@ function EarlyAccess(props: WithTokenPageProps) {
     create.mutate(newUser, {
       onSuccess: () => {
         if (SyncService.isEnabled) {
-          router.push('/login')
+          router.push('/signup')
           return
         }
         router.replace('/plan/new')
