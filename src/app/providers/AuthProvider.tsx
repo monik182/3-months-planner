12b621f822d9b1cb23ed5600cbfebc038f079dfd
Monica Carvajal <monik.182@gmail.com>
2025-07-13 18:13:47 +0200

    create new auth provider

---
+++ src/app/providers/AuthProvider.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { createContext, useState, useEffect, useContext, ReactNode } from 'react';
+import { Session, User } from '@supabase/supabase-js';
+import { createClient } from '@/app/util/supabase/client';
+
+type AuthContextType = {
+  user: User | null;
+  session: Session | null;
+  isLoading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  session: null,
+  isLoading: true,
+});
+
+export function AuthProvider({
+  children,
+  initialUser = null,
+}: {
+  children: ReactNode;
+  initialUser: User | null;
+}) {
+  const [user, setUser] = useState<User | null>(initialUser);
+  const [isLoading, setIsLoading] = useState(false);
+  const [session, setSession] = useState<Session | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    // Initialize with SSR data
+    setUser(initialUser);
+    setIsLoading(false);
+
+    // Listen for auth changes on the client
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user || null);
+        setSession(session);
+      }
+    );
+
+    return () => subscription?.unsubscribe?.();
+  }, [initialUser]);
+
+  if (typeof window === "undefined") return null;
+
+  return (
+    <AuthContext.Provider value={{ user, session, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
