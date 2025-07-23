b639c2f1d6c0eed11f8163a746322127df3ff5ef
Monik <monik182@users.noreply.github.com>
2025-07-24 00:41:56 +0200

    style(footer): darken footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { Box, Text } from '@chakra-ui/react'
 export function Footer() {
   return (
     <footer>
-      <Box paddingY="4" textAlign="center">
-        <Text fontSize="sm">
+      <Box paddingY="4" textAlign="center" bg="gray.200">
+        <Text fontSize="sm" color="gray.700">
           MC Code Studio Product © {new Date().getFullYear()}
         </Text>
       </Box>
